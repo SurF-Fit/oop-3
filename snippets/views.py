6f1717cb048e9a6de0102dc2212b52ad23d3d8af
SurF-Fit <kkulmenev@gmail.com>
2024-12-07 14:47:30 +0700

    create user, auth

--- snippets/views.py
+++ snippets/views.py
@@ -1,44 +1,31 @@
-from django.http import HttpResponse, JsonResponse
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
+from django.contrib.auth.models import User
+from rest_framework import generics
+from rest_framework import permissions
+
 from .models import Snippet
-from .serializers import SnippetSerializer
-
-@csrf_exempt
-def snippet_list(request):
-   if request.method == 'GET':
-       snippets = Snippet.objects.all()
-       serializer = SnippetSerializer(snippets, many=True)
-       return JsonResponse(serializer.data, safe=False)
-
-   elif request.method == 'POST':
-       data = JSONParser().parse(request)
-       serializer = SnippetSerializer(data=data)
-       if serializer.is_valid():
-           serializer.save()
-           return JsonResponse(serializer.data, status=201)
-       return JsonResponse(serializer.errors, status=400)
+from .serializers import SnippetSerializer, UserSerializer
 
 
-@csrf_exempt
-def snippet_detail(request, pk):
-   try:
-       snippet = Snippet.objects.get(pk=pk)
-   except Snippet.DoesNotExist:
-       return HttpResponse(status=404)
+class SnippetList(generics.ListCreateAPIView):
+   queryset = Snippet.objects.all()
+   serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
 
-   if request.method == 'GET':
-       serializer = SnippetSerializer(snippet)
-       return JsonResponse(serializer.data)
+   def perform_create(self, serializer):
+      serializer.save(owner=self.request.user)
 
-   elif request.method == 'PUT':
-       data = JSONParser().parse(request)
-       serializer = SnippetSerializer(snippet, data=data)
-       if serializer.is_valid():
-           serializer.save()
-           return JsonResponse(serializer.data)
-       return JsonResponse(serializer.errors, status=400)
 
-   elif request.method == 'DELETE':
-       snippet.delete()
-       return HttpResponse(status=204)
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+   queryset = Snippet.objects.all()
+   serializer_class = SnippetSerializer
+   permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+
+class UserList(generics.ListAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+   queryset = User.objects.all()
+   serializer_class = UserSerializer
+
